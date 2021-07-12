89fa1fd7bc5fc4b61ccc315a1df07d91aeb6d91d
Lukas Heise <lukas.heise@gmail.com>
2021-07-12 15:09:56 +0200

    Enable rendering of more overlays per frame

--- Horde3D/Source/Horde3DEngine/egRenderer.h
+++ Horde3D/Source/Horde3DEngine/egRenderer.h
@@ -28,7 +28,7 @@ class LightNode;
 class CameraNode;
 struct ShaderContext;
 
-const uint32 MaxNumOverlayVerts = 2048;
+const uint32 MaxNumOverlayVerts = 8192;
 const uint32 ParticlesPerBatch = 64;	// Warning: The GPU must have enough registers
 const uint32 QuadIndexBufCount = MaxNumOverlayVerts * 6;
 
